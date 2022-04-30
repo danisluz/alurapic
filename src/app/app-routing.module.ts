2458239be924fe6097814a32af28e7cbf1b8838a
daniel_luz <danisluz@gmail.com>
2022-04-30 17:08:31 -0300

    create photo form and add router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+
+const routes: Routes = [
+  { path: 'user/flavio', component: PhotoListComponent },
+  { path: 'p/add', component: PhotoFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,19 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
-  declarations: [ PhotoComponent ],
-  exports: [ PhotoComponent ],
-  imports: [ HttpClientModule ]
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent
+  ],
+  exports: [PhotoComponent],
+  imports: [
+    HttpClientModule,
+    CommonModule
+  ],
 })
 export class PhotosModule {}
