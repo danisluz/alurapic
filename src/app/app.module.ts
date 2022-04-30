16c6e286358b604e1c2e14cf8a97b825c2dc5f44
daniel_luz <danisluz@gmail.com>
2022-04-30 18:23:24 -0300

    create errors module and component not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorsModule } from './errors/errors.module';
 
 import { PhotosModule } from './photos/photos.module';
 
@@ -13,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     PhotosModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
