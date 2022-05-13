1a2cf8c7f1d270ebce2faa86a1af3123a62fe29d
daniel_luz <danisluz@gmail.com>
2022-05-13 14:50:21 -0300

    implements detect platafom brower

---
+++ src/app/core/platform-detector/platform-detector.service.ts
@@ -0,0 +1,15 @@
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+
+@Injectable({ providedIn: 'root'})
+export class PlataformDetectorService {
+
+  constructor( @Inject(PLATFORM_ID) private plataformId: string ){
+
+  }
+
+  isPlatformBrowser(){
+    return isPlatformBrowser(this.plataformId);
+  }
+
+}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/core/auth.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { PlataformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -9,12 +10,14 @@ import { AuthService } from 'src/app/core/auth.service';
 export class SignInComponent implements OnInit {
 
   loginForm!: FormGroup;
-  @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement> | undefined;
+  @ViewChild('userNameInput')
+  userNameInput!: ElementRef<HTMLInputElement>;
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private plataformDetector: PlataformDetectorService
   ) {}
 
   ngOnInit(): void {
@@ -36,7 +39,8 @@ export class SignInComponent implements OnInit {
             (err) => {
               console.log(err),
               this.loginForm.reset(),
-              this.userNameInput?.nativeElement.focus();
+              this.plataformDetector.isPlatformBrowser() &&
+                this.userNameInput?.nativeElement.focus();
               alert('Invalid user name or password')
             }
         );
