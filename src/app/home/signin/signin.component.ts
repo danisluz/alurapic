c268f362a913806ef80704b8ce1dfb5fa747503e
daniel_luz <danisluz@gmail.com>
2022-05-13 14:14:20 -0300

    implament focus login in

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
@@ -8,8 +9,13 @@ import { AuthService } from 'src/app/core/auth.service';
 export class SignInComponent implements OnInit {
 
   loginForm!: FormGroup;
+  @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement> | undefined;
 
-  constructor( private formBuilder: FormBuilder, private authService: AuthService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -26,12 +32,13 @@ export class SignInComponent implements OnInit {
     this.authService
         .authenticate(userName, password)
         .subscribe(
-            () => console.log('autenticado'),
+            () => this.router.navigate(['user', userName]),
             (err) => {
               console.log(err),
-              this.loginForm.reset()
+              this.loginForm.reset(),
+              this.userNameInput?.nativeElement.focus();
+              alert('Invalid user name or password')
             }
-
         );
   }
 }
