248b5906ac02c57693585c15ac2e7035e903b7c5
daniel_luz <danisluz@gmail.com>
2022-05-14 11:43:22 -0300

    implement user service

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'ap-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  user$: Observable<User>;
+  user: User | undefined;
 
+  constructor(userService: UserService) {
+    this.user$ = userService.getUser();
+    this.user$.subscribe((user) => (this.user = user));
+  }
 }
