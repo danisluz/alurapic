aaa9843b4b2053b2836fb9fa154c30dbcf809bf5
daniel_luz <danisluz@gmail.com>
2022-05-15 10:04:38 -0300

    implements logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
@@ -9,10 +10,16 @@ import { UserService } from '../user/user.service';
 })
 export class HeaderComponent {
   user$: Observable<User>;
-  user: User | undefined;
 
-  constructor(userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {
     this.user$ = userService.getUser();
-    this.user$.subscribe((user) => (this.user = user));
+  }
+
+  logout(){
+    this.userService.logout();
+    this.router.navigate(['']);
   }
 }
