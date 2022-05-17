d85ebc77edbff5316d823c18dffb85c0085c8b58
daniel_luz <danisluz@gmail.com>
2022-05-17 15:11:05 -0300

    implements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/auth/auth.guard';
-import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { HomeComponent } from './home/home.component';
-import { SignInComponent } from './home/signin/signin.component';
-import { SignUpComponent } from './home/signup/signup.component';
 
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
@@ -18,24 +16,15 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    component: HomeComponent,
-    canActivate: [ AuthGuard ],
-    children: [
-      {
-        path: '',
-        component: SignInComponent,
-      },
-      {
-        path: 'signup',
-        component: SignUpComponent,
-      }
-    ]
+    //loadChildren: './home/home.module#HomeModule',
+    loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule),
   },
   { path: 'user/:userName',
       component: PhotoListComponent,
       resolve: { photos: PhotoListResolver }
   },
-  { path: 'p/add', component: PhotoFormComponent },
+  { path: 'p/add', component: PhotoFormComponent, canActivate: [ AuthGuard ] },
+  { path: 'p/:photoId', component: PhotoDetailsComponent },
   { path: '**', component: NotFoundComponent },
 
 ];
