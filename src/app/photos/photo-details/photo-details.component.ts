bce2c5d2c1e2e9532b688e1623a1b53b5d6ed72a
daniel_luz <danisluz@gmail.com>
2022-05-18 11:09:23 -0300

    implements photo details

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,6 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   templateUrl: './photo-details.component.html'
 })
-export class PhotoDetailsComponent {}
+export class PhotoDetailsComponent implements OnInit{
+
+  photo$!: Observable<Photo>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private photoService: PhotoService
+    ){}
+
+  ngOnInit(): void {
+    this.photo$ = this.photoService.findById(
+      this.route.snapshot.params['photoId']
+    )
+  }
+}
