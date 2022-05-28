7dea89b9ca91fd27f88ad2e42bfdcb711b7355c4
daniel_luz <danisluz@gmail.com>
2022-05-28 16:43:28 -0300

    implements photo comment

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,11 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { PhotoModule } from '../photo/photo.module';
 import { PhotoDetailsComponent } from './photo-details.component';
-
+import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.directive';
 @NgModule({
-  declarations: [PhotoDetailsComponent],
-  exports: [ PhotoDetailsComponent ],
-  imports: [ CommonModule, PhotoModule ]
+  declarations: [
+    PhotoDetailsComponent,
+    PhotoCommentsComponent,
+    PhotoOwnerOnlyDirective
+  ],
+  exports: [
+    PhotoDetailsComponent,
+    PhotoCommentsComponent
+  ],
+  imports: [
+    CommonModule,
+    PhotoModule,
+    RouterModule,
+    ReactiveFormsModule,
+    VMessageModule
+  ]
 })
+
 export class PhotoDetailsModule {}
